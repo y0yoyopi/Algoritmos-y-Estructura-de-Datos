9e9c9b8da7fc361b07cdd8e4caedc29811f13c8a
y0yoyopi <165915247+y0yoyopi@users.noreply.github.com>
2024-10-08 22:07:43 -0500

    Update BST_w6.cpp

--- homework/BST_w6.cpp
+++ homework/BST_w6.cpp
@@ -2,8 +2,162 @@
 using namespace std;
 
 
-int main() {
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
 
-return 0;
 
+Node* createNode(int data)
+{
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+
+Node* insertNode(Node* root, int data)
+{
+    if (root == nullptr) { 
+        return createNode(data);
+    }
+
+    // Otherwise, recur down the tree
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    }
+    else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+
+    // return the (unchanged) node pointer
+    return root;
+}
+
+
+void inorderTraversal(Node* root)
+{
+    if (root != nullptr) {
+        inorderTraversal(root->left);
+        cout << root->data << " ";
+        inorderTraversal(root->right);
+    }
+}
+
+
+Node* searchNode(Node* root, int key)
+{
+    // Base Cases: root is null or key is present at root
+    if (root == nullptr || root->data == key) {
+        return root;
+    }
+
+    // Key is greater than root's key
+    if (root->data < key) {
+        return searchNode(root->right, key);
+    }
+
+    // Key is smaller than root's key
+    return searchNode(root->left, key);
+}
+
+
+Node* minValueNode(Node* node)
+{
+    Node* current = node;
+    // loop down to find the leftmost leaf
+    while (current && current->left != nullptr) {
+        current = current->left;
+    }
+    return current;
+}
+
+
+Node* deleteNode(Node* root, int data)
+{
+    if (root == nullptr)
+        return root;
+
+    // If the data to be deleted is smaller than the root's
+    // data, then it lies in the left subtree
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    }
+    // If the data to be deleted is greater than the root's
+    // data, then it lies in the right subtree
+    else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    }
+    // if data is same as root's data, then This is the node
+    // to be deleted
+    else {
+        // node with only one child or no child
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // node with two children: Get the inorder successor
+        // (smallest in the right subtree)
+        Node* temp = minValueNode(root->right);
+
+        // Copy the inorder successor's content to this node
+        root->data = temp->data;
+
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+int main()
+{
+
+    Node* root = nullptr;
+    // create a BST
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 70);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+
+
+    cout << "Inorder traversal of the given Binary Search "
+            "Tree is: ";
+    inorderTraversal(root);
+    cout << endl;
+
+
+    root = deleteNode(root, 20);
+    cout << "After deletion of 20: ";
+    inorderTraversal(root);
+    cout << endl;
+
+
+    root = insertNode(root, 25);
+    cout << "After insertion of 25: ";
+    inorderTraversal(root);
+    cout << endl;
+
+
+    Node* found = searchNode(root, 25);
+
+    if (found != nullptr) {
+        cout << "Node 25 found in the BST." << endl;
+    }
+    else {
+        cout << "Node 25 not found in the BST." << endl;
+    }
+
+    return 0;
 }
